5407c22cd7e51c9a6efc2303ab831ad766ca6041
doluwamu <dboyadeitan@gmail.com>
2023-09-15 21:02:17 +0100

    created a temporary solution for the navbar

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -7,7 +7,7 @@ export default {
 </script>
 
 <template>
-  <nav class="sticky container m-auto">
+  <nav class="sticky container m-auto py-3 px-2">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" class="flex items-center">
         <img src="../../assets/img/logo.png" alt="img" style="width: 120px; height: 30px" />
@@ -63,12 +63,12 @@ export default {
         id="navbar-cta"
       >
         <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
+          class="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0"
         >
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+              class="block py-2 pl-3 pr-4 text-white text-center active:bg-blue-700 rounded hover:bg-gray-100 md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               aria-current="page"
               >Home</a
             >
@@ -76,32 +76,32 @@ export default {
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 text-gray-900 text-center rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >Cars</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 text-gray-900 text-center rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >Support</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 text-gray-900 text-center rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >FAQ's</a
             >
           </li>
         </ul>
 
-        <div class="flex flex-col my-2 gap-3 md:flex-row md:hidden">
+        <div class="flex flex-col my-2 gap-3 items-center md:flex-row md:hidden">
           <a
             href="#"
             type="button"
-            class="button py-2 px-4 bg-black text-white rounded-lg"
-            style="max-width: 118px"
+            class="button py-2 px-4 bg-black text-white text-center rounded-lg"
+            style="width: 118px"
           >
             Get started
           </a>
@@ -109,7 +109,7 @@ export default {
             href="#"
             type="button"
             class="button py-2 px-4 bg-main text-white text-center rounded-lg"
-            style="max-width: 118px"
+            style="width: 118px"
           >
             Login
           </a>
